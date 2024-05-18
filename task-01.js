e778a14c32b305a9353cd71af2b5c2c8cb4e9f82
amper00 <141366407+amper00@users.noreply.github.com>
2024-05-18 16:28:34 +0200

    Update task-01.js

--- task-01.js
+++ task-01.js
@@ -1,8 +1,11 @@
 function slugify(title)
 {
  const slug = title.toLowerCase();
-return slug.join("-");
+ return slug.split("-");
 
 }
 
 console.log(slugify("Arrays for begginers")); 
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
